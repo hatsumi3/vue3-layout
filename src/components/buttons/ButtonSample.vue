31f55d875ba884f5153789aa10537a3c19a133ad
hatsumi3 <al.usamiku39@gmail.com>
2023-05-10 23:31:47 +0900

    feat: add ButtonSample

---
+++ src/components/buttons/ButtonSample.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <p class="mb-2 text-2xl">Buttons</p>
+        <div class="mb-2">
+            <PrimaryButton @click="handleClick">Primary</PrimaryButton>
+        </div>
+        <div class="mb-2">
+            <PrimaryButton :disabled="true" @click="handleClick">Disabled Primary</PrimaryButton>
+        </div>
+        <div class="mb-2">
+            <SecondaryButton @click="handleClick">Secondary</SecondaryButton>
+        </div>
+        <div class="mb-2">
+            <SecondaryButton :disabled="true" @click="handleClick">Disabled Secondary</SecondaryButton>
+        </div>
+        <div class="mb-2">
+            <SecondaryButton href="https://example.com" target="_blank" rel="noopener" @click="handleClick">Secondary link
+            </SecondaryButton>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import PrimaryButton from './PrimaryButton.vue';
+import SecondaryButton from './SecondaryButton.vue'
+
+function handleClick() {
+    alert("clicked")
+}
+
+</script>
---
+++ src/components/buttons/PrimaryButton.vue
@@ -0,0 +1,36 @@
+
+<template>
+    <BaseButton :disabled="disabled" :class="buttonClass" @click="handleClick">
+        <slot />
+    </BaseButton>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import BaseButton from "./BaseButton.vue";
+
+const props = defineProps({
+    disabled: { type: Boolean, default: false }
+})
+
+const emit = defineEmits<{
+    (e: "click"): void
+}>()
+
+function handleClick() {
+    emit("click")
+}
+
+const buttonClass = computed(() => {
+
+    return props.disabled ? 'text-gray-400 bg-gray-100' : 'text-gray-900 bg-gray-200'
+
+    // ref: object style.
+    // return {
+    //     'text-gray-400 bg-gray-100': props.disabled,
+    //     'text-gray-900 bg-gray-200': !props.disabled,
+    // }
+
+})
+
+</script>
