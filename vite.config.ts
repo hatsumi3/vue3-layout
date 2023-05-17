0e3dcd4be3d86fa9209ab71c70e9f62a4228aec8
hatsumi3 <al.usamiku39@gmail.com>
2023-05-17 16:41:11 +0900

    feat: add build settings.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,25 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import postcssNesting from 'postcss-nesting';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build:
+    process.env.NODE_ENV === 'development'
+      ? {}
+      : {
+          lib: {
+            entry: path.resolve(__dirname, 'src/index.ts'),
+            name: 'vue3-layout',
+          },
+          rollupOptions: {
+            external: ['vue'],
+            output: {
+              globals: {
+                vue: 'Vue',
+              },
+            },
+          },
+        },
 });
